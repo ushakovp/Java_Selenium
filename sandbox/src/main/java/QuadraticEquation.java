9f4fd17b73565d0fb05ea841f00038245ecd75dd
ushakovp <ushakovpb@gmail.com>
2021-05-15 20:27:28 +0400

    Updated Quadric Equation execution

--- sandbox/src/main/java/QuadraticEquation.java
+++ sandbox/src/main/java/QuadraticEquation.java
@@ -1,21 +1,34 @@
 public class QuadraticEquation {
 
-    private double a;
-    private double b;
-    private double c;
-    private int n;
+    private final double a;
+    private final double b;
+    private final double c;
+    private final int n;
 
     public QuadraticEquation(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
+
         double d = b * b - 4 * a * c;
-        if (d > 0) {
-            n = 2;
-        } else if (d == 0) {
-            n = 1;
+
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    n = -1;
+                } else
+                    n = 0;
+            } else {
+                n = 1;
+            }
         } else {
-            n = 0;
+            if (d > 0) {
+                n = 2;
+            } else if (d == 0) {
+                n = 1;
+            } else {
+                n = 0;
+            }
         }
     }
 
--- sandbox/src/test/java/QuadraticEquationTest.java
+++ sandbox/src/test/java/QuadraticEquationTest.java
@@ -4,20 +4,38 @@ import org.junit.jupiter.api.Test;
 public class QuadraticEquationTest {
 
     @Test
-    public void test0(){
+    public void test0() {
         QuadraticEquation e = new QuadraticEquation(1, 1, 1);
         Assertions.assertEquals(e.getN(), 0);
     }
 
     @Test
-    public void test1(){
+    public void test1() {
         QuadraticEquation e = new QuadraticEquation(1, 12, 36);
         Assertions.assertEquals(e.getN(), 1);
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         QuadraticEquation e = new QuadraticEquation(1, -2, -3);
-        Assertions.assertEquals(e.getN(),2);
+        Assertions.assertEquals(e.getN(), 2);
+    }
+
+    @Test
+    public void testLinear() {
+        QuadraticEquation e = new QuadraticEquation(0, 1, 1);
+        Assertions.assertEquals(e.getN(), 1);
+    }
+
+    @Test
+    public void testConstant() {
+        QuadraticEquation e = new QuadraticEquation(0, 0, 1);
+        Assertions.assertEquals(e.getN(), 0);
+    }
+
+    @Test
+    public void Zero() {
+        QuadraticEquation e = new QuadraticEquation(0, 0, 0);
+        Assertions.assertEquals(e.getN(), -1);
     }
 }

fd84e3a764f66458340bb9f43684b513f5d4c7fa
ushakovp <ushakovpb@gmail.com>
2021-05-16 13:20:26 +0400

    Correct objects comparison

--- addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ApplicationManager.java
@@ -21,12 +21,12 @@ public class ApplicationManager {
     }
 
     public void init() {
-        if (browser == BrowserType.CHROME) {
+        if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.FIREFOX) {
+        } else if (browser.equals(BrowserType.FIREFOX)) {
             System.setProperty("webdriver.gecko.driver", "C:\\browserDrivers\\geckodriver.exe");
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.IE) {
+        } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         }
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));

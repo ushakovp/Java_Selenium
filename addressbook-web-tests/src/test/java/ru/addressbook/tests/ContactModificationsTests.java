40606789f3b6fa69b3475b42ff0b4fb520557430
Pavel Ushakov <ushakovpb@gmail.com>
2021-05-22 15:59:27 +0400

    Check and execute pre-conditions for contacts

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactModificationsTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactModificationsTests.java
@@ -7,6 +7,9 @@ public class ContactModificationsTests extends TestBase {
 
     @Test
     public void testContactModification() {
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new ContactData("Test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test@test.ru", "test@test.ru", "test@test.ru", "test", "17", "December", "2021", "18", "October", "2020", "test3", "test", "test", "test"), true);
+        }
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactform(new ContactData("Test2", "test2", "test2", "test2", "test2", "test", "test", "test", "test", "test", "test", "test@test.ru", "test@test.ru", "test@test.ru", "test", "17", "December", "2021", "18", "October", "2020", null, "test", "test", "test"), false);
         app.getContactHelper().submitContactModification();

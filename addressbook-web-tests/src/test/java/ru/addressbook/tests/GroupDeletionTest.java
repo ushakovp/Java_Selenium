07494d22b30be344370fb4e6b8580fc919e67d58
ushakovp <ushakovpb@gmail.com>
2021-06-19 19:33:59 +0400

    Changed GroupDeletionTest

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupDeletionTest.java
@@ -4,20 +4,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTest extends TestBase {
 
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
         }
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroup();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assertions.assertEquals(before - 1, after);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assertions.assertEquals(before.size() - 1, after.size());
     }
 
 }

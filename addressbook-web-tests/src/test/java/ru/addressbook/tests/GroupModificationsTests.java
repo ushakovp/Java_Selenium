e5fe29f2b55bbf288c2de03b8bd2ff79d99ece20
ushakovp <ushakovpb@gmail.com>
2021-06-19 19:41:18 +0400

    Changed GroupModificationsTests

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationsTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationsTests.java
@@ -4,22 +4,24 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationsTests extends TestBase {
 
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
         }
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().modifySelectedGroup();
         app.getGroupHelper().fillGroupForm(new GroupData("test3", "test3", "test3"));
         app.getGroupHelper().updateSelectedGroup();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assertions.assertEquals(before, after);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assertions.assertEquals(before.size(), after.size());
 
 
     }

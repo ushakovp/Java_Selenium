572f03bb4fb6f9b39f070146971b52dbeef67b85
ushakovp <ushakovpb@gmail.com>
2021-06-20 12:28:06 +0400

    Changed GroupModificationsTests

--- addressbook-web-tests/src/test/java/ru/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/model/GroupData.java
@@ -1,29 +1,28 @@
 package ru.addressbook.model;
 
-import java.util.Objects;
-
 public class GroupData {
     private final String name;
+    private final String id;
     private final String header;
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
+        this.id = null;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
+    public GroupData(String id, String name, String header, String footer) {
+        this.name = name;
+        this.id = id;
+        this.header = header;
+        this.footer = footer;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
@@ -42,6 +41,25 @@ public class GroupData {
     public String toString() {
         return "GroupData{" +
                 "name='" + name + '\'' +
+                ", id='" + id + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
+        return id != null ? id.equals(groupData.id) : groupData.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        return result;
     }
 }
--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationsTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationsTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationsTests extends TestBase {
@@ -17,12 +18,15 @@ public class GroupModificationsTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().modifySelectedGroup();
-        app.getGroupHelper().fillGroupForm(new GroupData("test3", "test3", "test3"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test3", "test3", "test3");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().updateSelectedGroup();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assertions.assertEquals(before.size(), after.size());
 
-
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assertions.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }

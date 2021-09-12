89238f3a86b7abbedceab12b90116e7ae669f5e9
Pavel Ushakov <ushakovpb@gmail.com>
2021-09-12 15:26:07 +0400

    modify ContactCreationTests with records control

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactCreationTests.java
@@ -1,13 +1,28 @@
 package ru.addressbook.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.addressbook.model.ContactData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() {
-        app.getContactHelper().createContact(new ContactData("Test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test@test.ru", "test@test.ru", "test@test.ru", "test", "17", "December", "2021", "18", "October", "2020", "test3", "test", "test", "test"), true);
+        List<ContactData> before = app.getContactHelper().getContactList();
+        ContactData contact = new ContactData("Test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test@test.ru", "test@test.ru", "test@test.ru", "test", "17", "December", "2021", "18", "October", "2020", "test3", "test", "test", "test");
+        app.getContactHelper().createContact(contact, true);
+
+        List<ContactData> after = app.getContactHelper().getContactList();
+        before.add(contact);
+        Assertions.assertEquals(before.size(), after.size());
+
+        Comparator<? super ContactData> byFullName = Comparator.comparing(ContactData::getFullName);
+        before.sort(byFullName);
+        after.sort(byFullName);
+        Assertions.assertEquals(before, after);
     }
 
 
